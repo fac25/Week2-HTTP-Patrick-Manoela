70904ed2e057bf16d8cd404761a905e82aea4d6d
Patrick Delaney <zefiru@protonmail.com>
2022-06-15 20:20:35 +0100

    Move API file

---
+++ apis/spoonacular.js
@@ -0,0 +1,11 @@
+const API_KEY = "7b66bcffb0ff477498ca05be367d653a";
+
+async function getRecipeName(name) {
+  const query = await fetch(
+    `https://api.spoonacular.com/recipes/complexSearch?apiKey=${API_KEY}&query=${name}`
+  );
+  const data = await query.json();
+  return data;
+}
+
+export { getRecipeName };
