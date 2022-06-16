e096f3320cd953fa74b78a683345827d1a643bc5
Patrick Delaney <zefiru@protonmail.com>
2022-06-16 11:41:34 +0100

    Add basic sign in functionality

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 import { getRecipesByName } from "./apis/spoonacular.js";
 import { editDocument, getUsers } from "./apis/pantry.js";
 
-const input = document.querySelector("input");
-const button = document.querySelector("button");
+/*
+
+o---------------o
+| Recipe Search |
+o---------------o
+
+*/
+
+const input = document.querySelector(".search__input");
+const button = document.querySelector(".search__button");
 
 button.addEventListener("click", handleClick);
 
@@ -33,4 +41,58 @@ function createFromTemplate({ templateSelector, parentSelector, content }) {
   parent.append(newElement);
 }
 
-console.log(await getUsers());
+/*
+
+o---------o
+| Sign In |
+o---------o
+
+*/
+
+const signInBtn = document.querySelector(".sign-in");
+
+// User clicks 'sign in'
+signInBtn.addEventListener("click", toggleModal);
+
+// Modal pops up, asking for userName and password
+const modal = document.querySelector(".modal");
+
+// Display Modal
+function toggleModal() {
+  modal.classList.toggle("modal--active");
+}
+
+// Hide Modal
+const modalCloseBtn = document.querySelector(".modal__close");
+
+modalCloseBtn.addEventListener("click", toggleModal);
+
+// User clicks 'sign in' in the form to validate userName and password
+const formSignIn = document.querySelector(".modal__button");
+
+formSignIn.addEventListener("click", signIn);
+
+// Check if information is correct
+async function signIn() {
+  const username = document.querySelector("#username").value;
+  const password = document.querySelector("#password").value;
+
+  const users = await getUsers();
+  const currentUserData = users.find(
+    (user) => user.username.toLowerCase() === username.toLowerCase()
+  );
+
+  console.log(
+    currentUserData.password === password
+      ? "Signed In"
+      : "Wrong password and/or username"
+  );
+}
+
+/*
+
+o----------o
+| Register |
+o----------o
+
+*/
